dd23bddd8fe738efb19cd69305aeab78d204a284
Kutoon4ik <frognot52@gmail.ru>
2023-07-11 22:57:06 +0400

    123

---
+++ win2.py
@@ -0,0 +1,11 @@
+import typing
+from PyQt5 import QtCore
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QLabel, QApplication, QVBoxLayout, QWidget, QPushButton)
+
+from instr import *
+
+
+class TestWin(QWidget):
+    def __init__(self):
+        pass
