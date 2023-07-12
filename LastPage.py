1fcdac6658e12fdc54e0933f8b50a2b25a996504
Kutoon4ik <frognot52@gmail.ru>
2023-07-12 22:03:30 +0400

    sgf

---
+++ LastPage.py
@@ -0,0 +1,8 @@
+import typing
+from PyQt5 import QtCore
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QLabel, QApplication, QVBoxLayout, QWidget, QPushButton)
+from instr import *
+class LastPage(QWidget):
+    def __init__(self):
+        super().__init__()
